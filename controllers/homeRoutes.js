b039aea3bb51b41d8826af9d9338f83e3f02dff7
Zachary-Rossman <zrossman@yahoo.com>
2026-01-23 19:20:11 -0500

    Server now able to run.

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const { Car } = require('../models');
+// const withAuth = require('../utils/auth');
+
+// Prevent non logged in users from viewing the homepage
+// router.get('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const carData = await Car.findAll({
+    //   attributes: { exclude: ['password'] },
+    //   order: [['name', 'ASC']],
+    });
+
+    const cars = carData.map((car) => car.get({ plain: true }));
+
+    res.render('homepage', {
+      cars,
+      // Pass the logged in flag to the template
+    //   logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// router.get('/login', (req, res) => {
+//   // If a session exists, redirect the request to the homepage
+//   if (req.session.logged_in) {
+//     res.redirect('/');
+//     return;
+//   }
+
+//   res.render('login');
+// });
+
+module.exports = router;
